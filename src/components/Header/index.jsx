5ae781b30c984abe422f4d8cb6bb768f9ae439d6
daniel-barbosaa <danimendes9728@gmail.com>
2026-01-27 20:51:33 -0300

    chore: implements and adjust header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
-import Car from '../../assets/ico-car.png';
-import Pessoa from '../../assets/ico-pessoa.png';
+import { ShoppingCart, User } from 'lucide-react';
 import { useUser } from '../../hooks/UserContext';
-import {
-  Container,
-  ContainerLeft,
-  PageLink,
-  ContainerRight,
-  ContainerText,
-  Line,
-  PageLinkLogOut,
-} from './style';
+
+import { cn } from '@/lib/utils';
 
 export function Header() {
   const { pathname } = useLocation();
@@ -26,36 +18,56 @@ export function Header() {
   };
 
   return (
-    <Container>
-      <ContainerLeft>
-        <PageLink to="/" isactive={pathname === '/' ? 'true' : ''}>
+    <div className="h-18 bg-white shadow-md flex items-center justify-around flex-row ">
+      <div className="flex gap-6">
+        <Link
+          className={cn(
+            'cursor-pointer decoration-0',
+            pathname === '/'
+              ? 'text-violet-600 font-semibold'
+              : 'text-zinc-600',
+          )}
+          to="/"
+        >
           Home
-        </PageLink>
-        <PageLink
+        </Link>
+        <Link
+          className={cn(
+            'cursor-pointer decoration-0',
+            pathname === '/produtos'
+              ? 'text-violet-600 font-semibold'
+              : 'text-zinc-600',
+          )}
           to="/produtos"
-          isactive={pathname.includes('produtos') ? 'true' : ''}
         >
           Ver produtos
-        </PageLink>
-      </ContainerLeft>
+        </Link>
+      </div>
 
-      <ContainerRight>
-        <PageLink to="/carrinho">
-          <img src={Car} alt="logo-carrinho" />
-        </PageLink>
-        <Line />
-        <PageLink>
-          <img src={Pessoa} alt="logo-pessoa" />
-        </PageLink>
-        <ContainerText>
-          <p>
+      <div className="flex items-center gap-3">
+        <Link to="/carrinho">
+          <ShoppingCart className="text-violet-600" />
+        </Link>
+        <div className="h-10 border-l border-zinc-200" />
+
+        <Link to="/">
+          <User className="text-violet-600" />
+        </Link>
+        <div>
+          <p className="text-sm text-zinc-600 font-medium">
             Olá
             {', '}
             {userData.name}
           </p>
-          <PageLinkLogOut onClick={logOut}>Sair</PageLinkLogOut>
-        </ContainerText>
-      </ContainerRight>
-    </Container>
+          <button
+            type="button"
+            onClick={logOut}
+            className="text-sm text-violet-600 font-bold cursor-pointer"
+          >
+            Sair
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
